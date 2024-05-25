562db3a9878ebbf3bef82481e92dd54d482ba0d6
Baor74 <nvhbao1132@gmail.com>
2024-05-25 08:46:22 +0700

    sixth commit

--- src/main/java/com/example/demo/demo.java
+++ src/main/java/com/example/demo/demo.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class demo {
     @RequestMapping("/demo")
-    public String demo1(){
+    public String demo1() {
         return "detail";
     }
 }
