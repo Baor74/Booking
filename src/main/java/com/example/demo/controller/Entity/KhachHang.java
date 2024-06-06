1aa8bb8bf5526f3bd665cd56b86aa4bfe06ae2f6
Baor74 <nvhbao1132@gmail.com>
2024-06-06 21:05:31 +0700

    Hoang Bao Commit

--- src/main/java/com/example/demo/controller/Entity/KhachHang.java
+++ src/main/java/com/example/demo/controller/Entity/KhachHang.java
@@ -3,6 +3,8 @@ package com.example.demo.controller.Entity;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.util.Date;
+
 @Data
 @Getter
 @Setter
@@ -21,8 +23,23 @@ public class KhachHang {
     @Column(name = "Mat_khau")
     private String password;
 
-    @Column(name="ho_ten")
+    @Column(name="Ho_ten")
     private String fullname;
+
+    @Column(name = "Gioi_tinh")
+    private boolean gender;
+
+    @Column(name = "Ngay_sinh")
+    private Date birthday;
+
+    @Column(name = "Dia_chi")
+    private String address;
+
+    @Column(name = "Email" , unique = true)
+    private String email;
+
+    @Column(name = "STD", unique = true)
+    private String std;
 
 
 
--- src/main/java/com/example/demo/controller/Utils/AuthInterceptor.java
+++ src/main/java/com/example/demo/controller/Utils/AuthInterceptor.java
@@ -1,4 +1,5 @@
 package com.example.demo.controller.Utils;
+
 import com.example.demo.controller.Entity.KhachHang;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
