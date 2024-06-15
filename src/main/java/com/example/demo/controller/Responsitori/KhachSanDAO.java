25fd2dc01c0a27a14ecd8b3ca2cd57e699f1956c
Baor74 <nvhbao1132@gmail.com>
2024-06-15 20:47:58 +0700

    15/05 commit task

--- src/main/java/com/example/demo/controller/Responsitori/KhachSanDAO.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.controller.Responsitori;
-
-
-import com.example.demo.controller.Entity.KhachHang;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface KhachSanDAO extends JpaRepository<KhachHang, Integer> {
-}
